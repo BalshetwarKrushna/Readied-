b2f6880abd7200162234e80ebf1a1c0549af26a2
BalshetwarKrushna <balshetwark@gmail.com>
2024-08-07 18:05:50 +0530

    new uploded

---
+++ vehicle.java
@@ -0,0 +1,73 @@
+
+
+
+
+interface vehicle1{
+void gearchange();
+void speedup();
+void brake();
+}
+
+class Bicycle implements vehicle1{
+public void gearchange() {
+System.out.println("no gear");
+}
+
+public void speedup() {
+System.out.println("speed of bicycle ");
+}
+   public void  brake() {
+  System.out.println("bicycle has two brake");
+
+}
+}
+
+class car implements vehicle1{
+public void gearchange() {
+System.out.println(" five gear");
+}
+
+public void speedup() {
+System.out.println("speed of car is high ");
+}
+   public void  brake() {
+  System.out.println(" one brake");
+   }
+
+}
+class bike implements vehicle1{
+
+public void gearchange() {
+System.out.println(" four gear");
+}
+
+public void speedup() {
+System.out.println(" high speed of bike  is 140");
+}
+   public void  brake() {
+  System.out.println("  brake");
+   }
+
+
+
+}
+
+
+
+
+public class vehicle {
+public static  void main (String arg[]) {
+vehicle1 obj=new bike();
+obj.gearchange();
+obj.speedup();
+obj.brake();
+}
+
+}
+
+
+
+
+
+    
+}
